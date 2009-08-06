568f6f14650353ae50998417fc4c688d2d5024e3
Chris Mansley <cmansley@cs.rutgers.edu>
2009-08-06 13:53:27 -0400

    Modify test program

--- src/main.cc
+++ src/main.cc
@@ -29,9 +29,13 @@ int main(int argc, char* argv[])
     return -1;
   }
 
-
-  // rotate 90 deg
+  /*
+   * Run test program
+   */
+  roomba.Drive(500);
   roomba.Rotate(90);
+  roomba.Drive(300);
+  roomba.Rotate(-90);
 
   /*
    * Uninitialize the device
